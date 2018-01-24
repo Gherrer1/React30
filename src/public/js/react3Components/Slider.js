3f68b7ca81e7595cb3521c9c11b6310dd5d8c29b
Gerardo Herrera <gherrer1@binghamton.edu>
2018-01-24 18:46:53 -0500

    small bug related to defaultValue vs value prop for Slider Component

--- src/public/js/react3Components/Slider.js
+++ src/public/js/react3Components/Slider.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 const Slider = (props) => (
   <span>
     <label>{props.label}:</label>
-    <input type="range" min={props.min} max={props.max} value={props.value} onChange={(e) => props.sliderChanged(e.target.value)}></input>
+    <input type="range" min={props.min} max={props.max} value={props.defaultValue} onChange={(e) => props.sliderChanged(e.target.value)}></input>
   </span>
 );
 
