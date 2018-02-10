b7ffc60d7b7ca62b8c0ae6ecfd4d8a0c3398324d
Gerardo Herrera <gherrer1@binghamton.edu>
2018-02-10 04:46:35 -0500

    #10 (Hold Shift) React: Tested TodoItem component with Jest and Enzyme. Think I prefer Mocha. We will finish this mini project out with jest then might switch to mocha. Getting the hang of enzyme, super easy to pick up tool

--- src/components/components10/TodoItem.js
+++ src/components/components10/TodoItem.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 function TodoItem(props) {
+  const checkClassName = props.checked ? 'checked' : '';
   return (
-    <div>Hey</div>
+    <div className={`item ${checkClassName}`}>
+      <input type="checkbox" onClick={props.onCheck} />
+      <p>{props.text}</p>
+    </div>
   );
 }
 
--- src/components/components10/__tests__/TodoItem.test.js
+++ src/components/components10/__tests__/TodoItem.test.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import TodoItem from '../TodoItem';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+import sinon from 'sinon';
 
 describe('<TodoItem />', function() {
-  it('should render with a checkbox input', function() {
-    expect(2 + 2).toBe(4);
+  it('should have className = `item` ', function() {
+    const wrapper = shallow(<TodoItem />);
+    expect(wrapper.find('div').hasClass('item')).toBe(true);
   });
-  it('should render with a p');
-  it('should fill p with props.text');
-  it('should be styled differently if props.check === true');
+  it('should render with a checkbox input', function() {
+    const wrapper = shallow(<TodoItem />);
+    expect(wrapper.find('input[type="checkbox"]')).toHaveLength(1);
+  });
+  it('should render with a p', function() {
+    const wrapper = shallow(<TodoItem />);
+    expect(wrapper.find('p')).toHaveLength(1);
+  });
+  it('should fill p with props.text', function() {
+    const props = { text: 'Oh yeah' };
+    const wrapper = shallow(<TodoItem text={props.text} />);
+    expect(wrapper.find('p').text()).toEqual('Oh yeah');
+  });
+  it('should have div.className = `checked` if props.checked == true', function() {
+    const wrapper = shallow(<TodoItem checked={true} />);
+    expect(wrapper.find('div').hasClass('checked')).toBe(true);
+  });
+  it('should not have div.className = `checked` if props.checked == false', function() {
+    const wrapper = shallow(<TodoItem checked={false} />);
+    expect(wrapper.find('div').hasClass('checked')).toBe(false);
+  });
+  it('should call props.onCheck when input is checked', function() {
+    const cb = jest.fn();
+    const wrapper = shallow(<TodoItem onCheck={cb} />);
+    wrapper.find('input').simulate('click');
+    expect(cb).toHaveBeenCalledTimes(1);
+  });
 });
